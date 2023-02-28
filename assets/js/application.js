b2f3364d2dad5f1e98f49eb6030e0bbc6ae49c6b
usds <>
2023-02-28 21:06:59 +0000

    Update from https://github.com/usds/website/commit/81674d116f2cd477fac050a832dab5f7146a88e7

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,11 +1,4 @@
 ---
 # Javascipts!!!
 ---
-- {% include_relative vendor/jquery.js %}
-- {% include_relative vendor/slick.js %}
-
-- {% include_relative site/carousel.js %}
-- {% include_relative site/instagram.js %}
 - {% include_relative site/lazyload.js %}
-- {% include_relative site/console-message.js %}
-// - {% include_relative site/apply.js %}
